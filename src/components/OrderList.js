86e1c6ea476713bebc977f4be8a8841bf1a3fc15
mayank8aug <mayank8aug@gmail.com>
2020-01-31 12:04:35 +0530

    Added status translations

--- src/components/OrderList.js
+++ src/components/OrderList.js
@@ -3,12 +3,13 @@ import './Items.scss';
 const OrderList = props => {
     const { OrderData, localizationConfig = {} } = props;
     const { LABEL_BRAND, STATUS, ORDER_LABEL } = localizationConfig;
-    const modifyData = {'ORDER_PLACED' : 'ORDER PLACED'}
     return (
         <div className="order-section">
             <div className="card-heading">{ORDER_LABEL}</div>
             {OrderData.map(order => {
                 const { trackingDetails } = order;
+                const status = trackingDetails[0].status.replace('_', '');
+                const statusLabel = localizationConfig[status] || status;
                 return (
                     <div className="order-items" key={order.id}>
                         {
@@ -20,8 +21,8 @@ const OrderList = props => {
                                         <div className="display-flex flex-direction-column">
                                             <div className="margin-b5">{LABEL_BRAND} : {item.brandName}</div>
                                             <div className="display-flex fs12 text-gray flex-direction-column">
-                                                <div className="margin-r10">{modifyData[trackingDetails[0].status]}</div>
-                                                    {trackingDetails[0].status !== 'DELIVERED' ? <div className="">Expected Delivery date <span className="bold">{trackingDetails[0].details && trackingDetails[0].details[0].time}</span></div> : <div className="">{STATUS}: {trackingDetails[0].status}</div>}
+                                                {trackingDetails[0].status !== 'DELIVERED' && <div className="margin-r10">{statusLabel}</div>}
+                                                {trackingDetails[0].status !== 'DELIVERED' ? <div className="">Expected Delivery date <span className="bold">{trackingDetails[0].details && trackingDetails[0].details[0].time}</span></div> : <div className="">{STATUS}: {statusLabel}</div>}
                                             </div>
                                         </div>
                                     </div>
