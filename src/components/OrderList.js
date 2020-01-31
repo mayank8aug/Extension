c4a90a5b86a842f4586010f5d1a28c77b342776d
mayank8aug <mayank8aug@gmail.com>
2020-01-31 12:51:11 +0530

    Updated status keys

--- src/components/OrderList.js
+++ src/components/OrderList.js
@@ -7,13 +7,14 @@ const OrderList = props => {
         <div className="order-section">
             <div className="card-heading">{ORDER_LABEL}</div>
             {OrderData.map(order => {
-                const { trackingDetails } = order;
-                const status = trackingDetails[0].status.replace('_', '');
-                const statusLabel = localizationConfig[status] || status;
+                const { deliveryDate } = order;
                 return (
                     <div className="order-items" key={order.id}>
                         {
                             order.items.map(item => {
+                                const { status } = item;
+                                const statusKey = status.status.replace('_', '');
+                                const statusLabel = localizationConfig[statusKey] || statusKey;
                                 return (<React.Fragment key={item.id}>
                                     <div className="display-flex item" key={item.id}>
                                         <div className="image-container">
@@ -21,8 +22,8 @@ const OrderList = props => {
                                         <div className="display-flex flex-direction-column">
                                             <div className="margin-b5">{LABEL_BRAND} : {item.brandName}</div>
                                             <div className="display-flex fs12 text-gray flex-direction-column">
-                                                {trackingDetails[0].status !== 'DELIVERED' && <div className="margin-r10">{statusLabel}</div>}
-                                                {trackingDetails[0].status !== 'DELIVERED' ? <div className="">Expected Delivery date <span className="bold">{trackingDetails[0].details && trackingDetails[0].details[0].time}</span></div> : <div className="">{STATUS}: {statusLabel}</div>}
+                                                {statusKey !== 'DELIVERED' && <div className="margin-r10">{statusLabel}</div>}
+                                                {statusKey !== 'DELIVERED' ? <div className="">Expected Delivery date <span className="bold">{new Date(deliveryDate).toLocaleDateString()}</span></div> : <div className="">{STATUS}: {statusLabel}</div>}
                                             </div>
                                         </div>
                                     </div>
