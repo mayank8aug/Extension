d1689f15938ff756e6e48e6f0526d1429c2a8a77
nishantparashar52 <nishant.parashar@lenskart.in>
2020-01-31 12:35:38 +0530

    Added popup for extension

--- public/contentscript.js
+++ public/contentscript.js
@@ -7,30 +7,30 @@ if (!location.ancestorOrigins.contains(extensionOrigin)) {
     iframe.src = chrome.runtime.getURL('frame.html');
 
     // Some styles for a fancy sidebar
-    iframe.style.cssText = 'position:fixed;right:0;top:40%;display:block;' +
-                           'width:300px;height:100%;z-index:1000;';
+    iframe.style.cssText = 'position: fixed;right: 0px;top: 9%;display: block;width: 300px;height: 320px;z-index: 1000;' +
+                           'border: 1px solid rgb(204, 204, 204);transform: rotateY(0);z-index: 100;backface-visibility: hidden;transition: .6s;transform-style: preserve-3d;box-shadow: 0 1px 6px rgba(0,0,0,.3);';
     document.body.appendChild(iframe);
-    document.getElementById("theButton").addEventListener("click",
-    function() {
-  window.postMessage({ type: "FROM_PAGE", text: "Hello from the webpage!" }, "*");
-}, false);
+//     document.getElementById("theButton").addEventListener("click",
+//     function() {
+//   window.postMessage({ type: "FROM_PAGE", text: "Hello from the webpage!" }, "*");
+// }, false);
 }
 
-function removeIframe() {
-    debugger;
-    console.log(window);
-    document.getElementById('lenskartiFrame').remove();
-}
+// function removeIframe() {
+//     debugger;
+//     console.log(window);
+//     document.getElementById('lenskartiFrame').remove();
+// }
 // var port = chrome.runtime.connect();
 
-window.addEventListener("message", function(event) {
-  // We only accept messages from ourselves
-  if (event.source != window)
-    return;
+// window.addEventListener("message", function(event) {
+//   // We only accept messages from ourselves
+//   if (event.source != window)
+//     return;
 
-  if (event.data.type && (event.data.type == "FROM_PAGE")) {
-    console.log("Content script received: " + event.data.text);
-    debugger;
-    // port.postMessage(event.data.text);
-  }
-}, false);
+//   if (event.data.type && (event.data.type == "FROM_PAGE")) {
+//     console.log("Content script received: " + event.data.text);
+//     debugger;
+//     // port.postMessage(event.data.text);
+//   }
+// }, false);
